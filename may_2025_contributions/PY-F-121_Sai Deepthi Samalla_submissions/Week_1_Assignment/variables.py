54e329f90e8cfc2afb314e16a3a47ba4bda1c604
Sai Deepthi Samalla <deepthisamalla1111@gmail.com>
2025-06-01 15:30:13 +0530

    completed variables assignment

---
+++ may_2025_contributions/PY-F-121_Sai Deepthi Samalla_submissions/Week_1_Assignment/variables.py
@@ -0,0 +1,7 @@
+# Assignment: Variables
+# Task: Create two variables, first_name and last_name, assign your own name to them,
+# then print the full name on one line.
+
+first_name = "Sai Deepthi"
+last_name = "Samalla"
+print (first_name , last_name)

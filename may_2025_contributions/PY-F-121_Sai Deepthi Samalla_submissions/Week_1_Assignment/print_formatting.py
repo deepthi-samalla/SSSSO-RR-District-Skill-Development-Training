0ccfaeb9913aab655435b6eb9d1adf82611d5bea
Sai Deepthi Samalla <deepthisamalla1111@gmail.com>
2025-06-15 15:47:46 +0530

    Added print formatting assignment

---
+++ may_2025_contributions/PY-F-121_Sai Deepthi Samalla_submissions/Week_1_Assignment/print_formatting.py
@@ -0,0 +1,6 @@
+# Assignment: Print() formatting
+# Task: Print the numbers 1 to 5 on one line separated by commas using a single print() call.
+
+# Print the numbers 1 to 5 using a single print() call
+# The 'sep=','' argument tells print() to put a comma between each item
+print(1, 2, 3, 4, 5, sep=',')
